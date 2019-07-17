3f8377f9dc9de8612e1d1acc49876246c4c8c3fa
yreztsov <yevgen@holonis.com>
2019-07-16 23:38:16 -0700

    cleanup server, add launch message

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const { port, logger } = require('./config/server');
 // eslint-disable-next-line import/order
-const fastify = require('fastify')({ logger }); // Per requirements, no logging.
-const notesController = require('./controllers/NotesController');
+const fastify = require('fastify')({ logger });
+const notesController = require('./controllers/NoteController');
 
 notesController.setFastifyInstance(fastify);
-notesController.registerRoutes(null);
+notesController.registerRoutes();
 
 fastify.listen(port);
+console.log(`Notes listening on port ${port}`);
